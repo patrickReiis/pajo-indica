264b40cf00ca16670cb38d4a469ca3fe1dca912f
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-24 22:12:25 -0300

    Validation file that has a function to see if the request.body is a valid Book. This file can also contain other validations in the future

---
+++ backend/src/validation.ts
@@ -0,0 +1,41 @@
+import type { Book } from './types';
+
+export function isBook(obj:any):boolean{
+
+    if (!obj || typeof obj !== 'object' || Array.isArray(obj)) {
+        return false
+    }
+    
+
+    const userObjKeys = Object.keys(obj);
+
+    const mockBook: Book = {title:'', author:'', genre:'poesia', keywords: []} // Used for reference since types doesn't exist at runtime
+    const mockBookKeys = Object.keys(mockBook);
+
+    if (userObjKeys.length !== mockBookKeys.length) return false;
+
+    if (Array.isArray(obj.keywords) === false) return false;
+
+    for (let i = 0; i < userObjKeys.length; i++) {
+        if ((userObjKeys[i] in mockBook) === false) { // 'X in object' checks if object has the property X
+            return false
+        } 
+
+    }
+
+    // Checking if the keywords array is only made of strings
+    for (let i = 0; i < obj.keywords.length; i++) {
+        if (typeof obj.keywords[i] !== 'string') {
+            return false
+        }
+    } 
+
+    // Checking if the values of the properties of the object are strings
+    for (const i in obj) { 
+        if (i == 'keywords') continue;
+        if (typeof obj[i] !== 'string') return false;
+    }
+
+
+    return true
+}
