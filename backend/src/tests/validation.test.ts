f3780ad6dbbfe8d76832ab9b59f3d0d6cd4f1182
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-24 22:13:23 -0300

    Tests for the validation file, for now it only tests the isBook(() function

---
+++ backend/src/tests/validation.test.ts
@@ -0,0 +1,34 @@
+import { isBook } from '../validation';
+import type { Book } from '../types';
+
+
+describe('Testing if the user input is a valid Book', () => {
+
+    test('Must be a book by isBook() returning true', () => {
+        const fakeBook: Book = {title:'', author:'', genre:'poesia', keywords: []}
+       expect(isBook(fakeBook)).toEqual(true); 
+    }) 
+
+    test('Must NOT be a book by isBook() returning false', () => {
+        const fakeBook: Book|any = {title:'', author:'', genre:'poesia', keywords: [], car: 'anything'} // object has more properties than the Book type
+        expect(isBook(fakeBook)).toEqual(false); 
+
+        const fakeBook2: Book|any = {title:'', author:''} // object misses some Book properties 
+
+        expect(isBook(fakeBook2)).toEqual(false); 
+
+        const fakeBook3: Book|any = {title:'', author:'', genre:'poesia', keywords: 'not array' } // keywords is not array 
+
+        expect(isBook(fakeBook3)).toEqual(false); 
+        
+        const fakeBook4: Book|any = {title:'', author:'', genre:'poesia', keywords: [1, 'any message :D'] } // keywords is not an array of strings 
+
+        expect(isBook(fakeBook4)).toEqual(false); 
+
+        const fakeBook5: Book|any = {title: null, author:2, genre: false, keywords: [ 'any message :D'] } // object keys except for 'keywords' properties are not strings 
+
+        expect(isBook(fakeBook5)).toEqual(false); 
+
+    }) 
+
+})
