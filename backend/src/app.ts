dff0151c3b102b4d9f8b46cfe1e763a3339b826e
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-24 22:09:14 -0300

    Calls isBook() function to validate if the request is a valid book

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,7 @@
 import express, {Application, Request, Response } from 'express';
+import { isBook } from './validation';
+import { notValidBookObj } from './messages';
+
 
 export const app:Application = express();
 app.use(express.json());
@@ -9,6 +12,11 @@ app.get('/', (req:Request, res:Response) => {
 })
 
 app.post('/api/v1/book/register', (req: Request, res: Response) => {
-    console.log(req.body)
-    res.end('Test')
+    if (isBook(req.body) === false) {
+        res.writeHead(400, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(notValidBookObj))
+        return
+    }
+
+    res.end('It is book')
 })
