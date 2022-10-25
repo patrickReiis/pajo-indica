d058833bd5ff006e6ff5dbbaad43f3974986c23e
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-24 22:04:52 -0300

    Message explaining that the body request is an invalid Book type

--- backend/src/messages.ts
+++ backend/src/messages.ts
@@ -4,3 +4,8 @@ export const targetNotFound:ErrorDb = {
     errorMsg: ['Hum... Não encontramos esse livro em nosso banco de dados.', 'Que tal registrar ele e depois voltar aqui?'],
     code: 'target-not-found'
 }
+
+export const notValidBookObj: ErrorDb = {
+    errorMsg: ['This is an invalid Book object.', 'The Book object is {title: string; genre: string[]; author: string; keywords: string[]'],
+    code: 'object-is-not-book'
+}
