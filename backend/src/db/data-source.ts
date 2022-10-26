a1e949781f8bc73c4a98b95bb770aa7689d6c028
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-26 09:51:13 -0300

    TypeORM database configuration

---
+++ backend/src/db/data-source.ts
@@ -0,0 +1,19 @@
+import * as dotenv from 'dotenv'; 
+dotenv.config();
+
+import { DataSource } from 'typeorm';
+import { Book } from './entity/Book';
+
+export const dataSource = new DataSource({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: 'pajo_db',
+    synchronize: true,
+    logging: true,
+    entities: [Book],
+    subscribers: [],
+    migrations: [],
+})
