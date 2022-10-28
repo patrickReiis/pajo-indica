dfd6f99c68b39cbe28d9fce52cfda5eca145cba2
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-28 10:44:08 -0300

    Book registration funcionality added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,64 @@
 import express, {Application, Request, Response } from 'express';
 import { isBook } from './validation';
-import { notValidBookObj } from './messages';
-
+import { notValidBookObj, bookAlreadyExist } from './messages';
+import { getImgFileType, createImageFile } from './custom-utils';
+import { dataSource } from './db/get-data-source';
+import { Book } from './db/entity/Book';
 
 export const app:Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req:Request, res:Response) => {
+
     res.end('Hi');
 })
 
-app.post('/api/v1/book/register', (req: Request, res: Response) => {
-    if (isBook(req.body) === false) {
+app.post('/api/v1/book/register', async (req: Request, res: Response) => {
+
+    const bodyData = req.body 
+
+    if (isBook(bodyData) === false) {
         res.writeHead(400, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(notValidBookObj))
         return
     }
 
-    res.end('It is book')
+    try {
+
+        const bookRepo = await dataSource.getRepository(Book);
+        
+        const book = new Book();
+        book.author = bodyData.author; 
+        book.title = bodyData.title; 
+        book.genre = bodyData.genre; 
+        book.keywords = bodyData.keywords; 
+
+        const bookSaved = await bookRepo.save(book); // Saving so it can get the id
+        const bookId = bookSaved.id;
+
+        const imgType =  getImgFileType(bodyData.imageBase64);
+        book.imagePath = `media/${bookId}.${imgType}`; 
+
+        await bookRepo.save(book);
+
+        createImageFile(book.imagePath, bodyData.imageBase64)
+        res.end('Livro registrado com sucesso!')
+        return
+
+    }
+    catch (err:any) {
+        if (err.code == '23505'){ // This code means 'unique_violation' - See https://www.postgresql.org/docs/current/errcodes-appendix.html
+            res.writeHead(403, {'Content-Type': 'application/json'}); // a request might be forbidden for reasons unrelated to credentials
+            res.end(JSON.stringify( { error:  bookAlreadyExist.errorMsg })) 
+            return
+        } 
+        else {
+            console.log(err);
+            res.writeHead(500);
+            res.end('Internal Error')
+            return
+        }
+
+    }
 })
