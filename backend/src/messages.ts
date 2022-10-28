8341faa27b5c6a0d9d4fc849c22e983d2588243e
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-28 10:45:36 -0300

    Book already created message.

--- backend/src/messages.ts
+++ backend/src/messages.ts
@@ -9,3 +9,8 @@ export const notValidBookObj: ErrorDb = {
     errorMsg: ['This is an invalid Book object.', 'The Book object is {title: string; genre: string[]; author: string; keywords: string[]'],
     code: 'object-is-not-book'
 }
+
+export const bookAlreadyExist: ErrorDb = {
+    errorMsg: ['Esse livro já está cadastrado em nosso sistema, vá até a barra de pesquisa para encontrá-lo.'],
+    code: 'book-already-exist'
+}
