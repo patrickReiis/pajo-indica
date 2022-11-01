84fd2cdfccf1c8050584eace82230972bb3d0bfb
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-01 16:45:23 -0300

    How it works component created

---
+++ frontend/src/components/HowItWorks.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1>vc faz isso e isso</h1>
+</template>
+
+<style scoped>
+
+@media screen and (min-width:0px) and (max-width:700px){
+}
+</style>
