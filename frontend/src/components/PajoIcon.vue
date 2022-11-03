b69206f982705291a4f78c1bce9aeebb7c1ca175
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-03 18:59:39 -0300

    Pajo icon component created

---
+++ frontend/src/components/PajoIcon.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="pajo-mini-logo">
+        <a href="#/home">
+            <h1>
+                Pajó
+            </h1>
+            <h1>
+                Indica
+            </h1>
+        </a>
+    </div>
+</template>
+<style scoped>
+
+.pajo-mini-logo {
+    font-family: 'Oswald', sans-serif;
+    line-height: 2.5rem;
+    border-radius: 0.35rem 1rem 1rem 0.35rem;
+    padding: 0.25rem;
+    margin-top: 0.35rem;
+}
+</style>
