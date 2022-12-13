2062772a12d83a9fe4553fbfa5ce07bf1387a2cd
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-13 14:30:08 -0300

    Getting books from the database instead of dummy JSON database

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -1,11 +1,22 @@
-import books from './books.json';
-import type { Book } from '../types';
+import type { Book as BookType} from '../types';
+import { dataSource } from './get-data-source';
+import { Book } from './entity/Book';
 
-export function getAllBooksFromDb(): Book[]{
-    return books as Book[]; 
+// think of image path and image base 64, also ID
+export async function getAllBooksFromDb(): Promise<BookType[]>{
+    const books = await dataSource.getRepository(Book).find({
+        select: {
+            title: true,
+            author: true,
+            imagePath: true,
+            keywords: true,
+            genre: true
+        }
+    })
+    return books as  BookType[]; 
 }
 
-export function getBookTargetFromDb(target: string):Book|null{
-    const targetBook = getAllBooksFromDb().filter(e => e.title == target)[0]
+export async function getBookTargetFromDb(target: string):Promise<Book|null>{
+    const targetBook = await dataSource.getRepository(Book).findOneBy({title: target});
     return targetBook ? targetBook : null 
 }
