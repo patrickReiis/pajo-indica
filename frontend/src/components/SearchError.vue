d1687a941e974183c9227ae6db9a76e97c10fe3c
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-17 13:46:40 -0300

    Component that renders error messages

---
+++ frontend/src/components/SearchError.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps(['messages']);
+</script>
+
+<template>
+    <div class="error-container" v-if="props.messages.length">
+        <img src="../assets/sad face.jpeg" alt="sad face" width=100 height=100>
+        <p v-for="message in messages" :key="message">
+            {{ message }}
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.error-container {
+    text-align: center;
+    font-size: 1.5rem;
+}
+</style>
